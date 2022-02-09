cf63f7f4ca8c2b937787d22e213a757b933b5f7a
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-10 00:37:36 +0530

    [Nikhil Kumar]:String Replace Program Added

---
+++ CorePrograms/Hello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePrograms
+{
+    public class Hello
+    {
+        public void Replace()
+        {
+            Console.WriteLine("Enter your Username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("“Hello {0},How are you?”", username);
+
+
+        }
+    }
+}
--- CorePrograms/Program.cs
+++ CorePrograms/Program.cs
@@ -11,6 +11,34 @@ namespace CorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Core Programs");
+            Console.WriteLine("Enter problem statement:" +
+                              "\n 1)Hello Username" +
+                              "\n 2)Exit");
+
+            
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Choice");
+                int n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        Hello obj1 = new Hello();
+                        obj1.Replace();
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+                }
+            }
         }
+            
     }
 }
